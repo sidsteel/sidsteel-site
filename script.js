9ad6ef07fe4e0864bff0ceb074676cebd9fa1d96
sidsteel <vladislavsidorov514@gmail.com>
2025-07-08 21:13:19 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('orderForm').addEventListener('submit', function (e) {
-  e.preventDefault();
-  this.style.display = 'none';
-  document.querySelector('.success').style.display = 'block';
-});
-document.getElementById('openForm').addEventListener('click', function () {
-  document.getElementById('formSection').scrollIntoView({ behavior: 'smooth' });
-});
